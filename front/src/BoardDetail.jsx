9822c2f97cc63866743754923c3f4409ab58a924
chlehdwon <cookie000215@gmail.com>
2022-07-18 20:18:34 +0000

    Rollback done

--- front/src/BoardDetail.jsx
+++ front/src/BoardDetail.jsx
@@ -12,6 +12,7 @@ class BoardDetail extends Component {
 
   componentDidMount() {
     if (this.props.location.query !== undefined) {
+      console.log(this.props.location.query._id)
       this.getDetail();
     } else {
       window.location.href = "/";
@@ -52,6 +53,7 @@ class BoardDetail extends Component {
       //정상 수행
       .then(returnData => {
         if (returnData.data.board[0]) {
+          console.log(returnData.data.board[0])
           const board = (
             <div>
               <Table striped bordered hover>
@@ -77,7 +79,17 @@ class BoardDetail extends Component {
                     query: {
                       title: returnData.data.board[0].title,
                       content: returnData.data.board[0].content,
-                      _id: this.props.location.query._id
+                      _id: this.props.location.query._id,
+                      user_id: returnData.data.board[0].user_id,
+                      user_pwd: returnData.data.board[0].user_pwd,
+                      qtitle: returnData.data.board[0].qtitle,
+                      qcontent: returnData.data.board[0].qcontent,
+                      qtag: returnData.data.board[0].qtag,
+                      stackoverflow: returnData.data.board[0].stackoverflow,
+                      serverfault: returnData.data.board[0].serverfault,
+                      superuser: returnData.data.board[0].superuser,
+                      askubuntu: returnData.data.board[0].askubuntu,
+                      askdifferent: returnData.data.board[0].askdifferent,
                     }
                   }}
                 >
