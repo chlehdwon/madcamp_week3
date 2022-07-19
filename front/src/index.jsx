216f6ed6b0321762dcaebcdc33d6799cd0d43e2c
chlehdwon <cookie000215@gmail.com>
2022-07-19 04:25:20 +0000

    Reupdate front

---
+++ front/src/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {HashRouter} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header";
+// import BoardForm from "./BoardForm";
+// import MypageForm from "./MypageForm";
+// import BoardWriteForm from "./BoardWriteForm";
+import Body from "./Body";
+import Footer from "./Footer";
+
+ReactDOM.render(
+  <HashRouter>
+    <Header/>
+    <Body/>
+    <Footer/>
+  </HashRouter>,
+  document.querySelector("#container")
+);
