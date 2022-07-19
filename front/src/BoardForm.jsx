77cbf3fbc2d77467dd3f4d172124d4bc30b9a7fd
Yoon Changho <ho9938@unist.ac.kr>
2022-07-19 07:05:17 +0000

    Change fonts

--- front/src/BoardForm.jsx
+++ front/src/BoardForm.jsx
@@ -3,7 +3,7 @@ import { Table } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
 import $ from "jquery";
-import {} from "jquery.cookie";
+import { } from "jquery.cookie";
 axios.defaults.withCredentials = true;
 const headers = { withCredentials: true };
 
